bfb59e2c828c3d3e4607a12b7deb206b0b1ba751
chokomancarr <puakai2010@gmail.com>
2019-06-03 13:49:35 +0900

    update icon, fix rpath not recursed update

--- modify_rpath.py
+++ modify_rpath.py
@@ -2,7 +2,7 @@ import inspect, os, sys
 import subprocess
 from shutil import copyfile
 
-def add_from(path, fd, lst, chg):
+def add_from(path, fd, lst):
     result = subprocess.run(['otool', '-L', path], stdout=subprocess.PIPE)
     strs = result.stdout.decode('utf-8')
     for s in strs.splitlines():
@@ -14,23 +14,35 @@ def add_from(path, fd, lst, chg):
         if ss[0].startswith("/usr/local/"):
             nm = ss[0][ss[0].rfind('/')+1:]
             if (nm == 'Python'):
-                continue
-            if nm in lst:
-                continue
-            lst.append(nm)
-            print('copying ' + nm + ' (' + ss[0] + ')')
-            copyfile(ss[0], fd + nm)
-            if chg:
-                subprocess.run(['install_name_tool', '-change', ss[0], '@rpath/' + nm, fd + 'avis'])
-            add_from(ss[0], fd, lst, False)
+                subprocess.run(['install_name_tool', '-change', ss[0], '@rpath/Python.framework/Versions/3.7/Python', path])
+            
+            else:
+                subprocess.run(['install_name_tool', '-change', ss[0], '@rpath/' + nm, path])
+                if nm in lst:
+                    continue
+                lst.append(nm)
+                print('copying ' + nm + ' (' + ss[0] + ')')
+                copyfile(ss[0], fd + nm)
+                add_from(fd + nm, fd, lst)
 
 basefd = (os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 fd = basefd + "/package/mac/AViS.app/Contents/MacOS/"
 
-add_from(fd + 'avis', fd, [], True)
+copied_libs = []
+
+add_from(fd + 'avis', fd, copied_libs)
+
+print('copying libRadeonRays.dylib')
+copyfile(basefd + '/radeonrays/build/bin/libRadeonRays.dylib', fd + 'libRadeonRays.dylib')
+print('copying libOpenImageDenoise.0.dylib')
+copyfile(basefd + '/oidn/build/libOpenImageDenoise.0.dylib', fd + 'libOpenImageDenoise.0.dylib')
+add_from(fd + 'libOpenImageDenoise.0.dylib', fd, copied_libs)
 
 #look for dylibs in own directory
 print('configuring rpath')
-subprocess.run(['install_name_tool', '-add_rpath', '\"@executable_path\"', fd + 'avis'])
+subprocess.run(['install_name_tool', '-add_rpath', '@executable_path', fd + 'avis'])
 
 print('files written to ' + fd)
+
+subprocess.run(['touch', basefd + '/package/mac/AViS.app'])
+subprocess.run(['touch', basefd + '/package/mac/AViS.app/Contents/Info.plist'])
