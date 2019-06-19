6e547b51671252db9e2b1138fc761a2d50e99ff6
Chokomancarr <puakai2010@gmail.com>
2019-06-19 15:28:27 +0900

    downsample test 3d

--- src/web/nodes/conv/node_downsample.cpp
+++ src/web/nodes/conv/node_downsample.cpp
@@ -1,5 +1,7 @@
 #include "node_downsample.h"
 
+#define RETERR(msg) { std::cerr << msg << std::endl; return; }
+
 INODE_DEF(__("Downsample"), Downsample, CONV);
 
 Node_Downsample::Node_Downsample() : INODE_INIT {
@@ -15,7 +17,51 @@ Node_Downsample::Node_Downsample() : INODE_INIT {
 }
 
 void Node_Downsample::Execute() {
-    return;
+	if (!inputR[0].first) return;
+    const auto ratio = getval_i(1);
+	if (ratio < 1)
+		RETERR("downsample ratio must be a positive integer!");
+
+    std::cout << "Downsampling to ";
+    const auto dim = dims.size();
+    auto sz = 1;
+    std::vector<int> szls(dim);
+    for (size_t a = 0; a < dim; a++) {
+        const auto dl = inputR[0].getdim(a);
+        szls[a] = dl;
+        sz *= (dims[a] = (dl + ratio - 1) / ratio + 1);
+        std::cout << dims[a] << " ";
+    }
+    std::cout << std::endl;
+
+    data.clear();
+    data.resize(sz);
+
+    assert(dim == 3);
+
+    const float nr = 1.f / std::pow(ratio, 3);
+    const int nxl = szls[0];
+    const int nyl = szls[1];
+    const int nzl = szls[2];
+    const int nyzl = nyl * nzl;
+    const int nz = dims[2];
+    const int nyz = dims[1] * nz;
+    const double* input = *(double**)inputR[0].getval(ANVAR_ORDER::C);
+    #pragma omp parallel for //collapse(3)
+    for (int x = 0; x < nxl; x++) {
+        for (int y = 0; y < nyl; y++) {
+            for (int z = 0; z < nzl; z++) {
+                int ix = (x + ratio - 1) / ratio;
+                int iy = (y + ratio - 1) / ratio;
+                int iz = (z + ratio - 1) / ratio;
+                data[ix * nyz + iy * nz + iz] += input[x * nyzl + y * nzl + z] * nr;
+            }
+        }
+    }
+
+	auto& ov = ((DmScript_I*)script.get())->outputVs;
+	ov[0].val.p = data.data();
+	ov[0].pval = &ov[0].val.p;
 }
 
 void Node_Downsample::OnConn(bool o, int i) {
